f613565d213a0f7ebf9de0b8a0b2be1d3237c8e5
Anirudh <anirudhashok8@gmail.com>
2024-12-17 08:40:12 +0530

    added list,update,delete api

--- expenseapi/urls.py
+++ expenseapi/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from expenseapi import views 
 
 urlpatterns=[
-    path('signup/',views.UserCreateView.as_view())
+    path('signup/',views.UserCreateView.as_view()),
+    path('expense/',views.ExpenseListView.as_view()),
+    path('expense/<int:pk>',views.ExpenseRetriveUpdateDelte.as_view()),
 ]
--- expenseapi/views.py
+++ expenseapi/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from expense.models import Expense
 from rest_framework.views import APIView
-from rest_framework import status
+from rest_framework import status,authentication,permissions
 from rest_framework.response import Response
-from expenseapi.serializer import UserSerializer
+from expenseapi.serializer import UserSerializer,ExpenseSerializer
+from rest_framework import serializers
+
 
 
 
@@ -21,3 +24,89 @@ class UserCreateView(APIView):
         
         return Response(data=serializer_instance.errors,status=status.HTTP_400_BAD_REQUEST)
     
+class ExpenseListView(APIView):
+    
+    authentication_classes=[authentication.BasicAuthentication]
+    permissions_calsses=[permissions.IsAdminUser]
+    
+    serializer_class=ExpenseSerializer
+  
+    def get (self,request,*args,**kwargs):
+        
+        qs=Expense.objects.filter(owner=request.user)
+        
+        if "status" in request.query_params:
+            
+            search_text=request.query_params.get("status")
+            
+            qs=qs.filter(status=search_text)
+        
+        serializer_instance=self.serializer_class(qs,many=True)
+        
+        return Response(data=serializer_instance.data,status=status.HTTP_201_CREATED)
+
+    def post(self,request,*args,**kwargs):
+        
+        serializer_instance=self.serializer_class(data=request.data)
+        
+        if serializer_instance.is_valid():
+            
+            serializer_instance.save(owner=request.user)
+            
+            return Response(data=serializer_instance.data)
+        
+        return Response(data=serializer_instance.errors)
+    
+    
+class ExpenseRetriveUpdateDelte(APIView):
+    
+    authentication_classes=[authentication.BasicAuthentication]
+    permission_classes=[permissions.IsAuthenticated]
+    
+    serializer_class=ExpenseSerializer
+    
+    def get(self,request,*args,**kwargs):
+        
+        id=kwargs.get('pk')
+        
+        qs=get_object_or_404(Expense,id=id)
+        
+        serializer_class=self.serializer_class(qs)
+        
+        return Response(data=serializer_class.data,status=status.HTTP_201_CREATED)
+    
+    def delete(self,request,*args,**kwargs):
+        
+        id=kwargs.get("pk")
+        
+        todo_obj=get_object_or_404(Expense,id=id)
+        
+        if request.user!=todo_obj.owner:
+            
+            raise serializers.ValidationError("access denied")
+        
+        todo_obj.delete()
+        
+        return Response(data={"message":"deleted"})
+    
+    def put(self,request,*args,**kwargs):
+        
+        id=kwargs.get('pk')
+        
+        todo_obj=get_object_or_404(Expense,id=id)
+        
+        if request.user != todo_obj.owner:
+            raise serializers.ValidationError("acess denied")
+        
+        serializer_instance=self.serializer_class(data=request.data,instance=todo_obj)
+        
+        if serializer_instance.is_valid():
+
+            serializer_instance.save(owner=request.user)
+            
+            return Response(data=serializer_instance.data)
+        
+        return Response(data=serializer_instance.errors)
+            
+        
+        
